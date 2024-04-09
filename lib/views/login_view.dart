aeb134c757a6b393bce2cf15a509c739c9f9836e
SurajTapkir05 <surajtapkir2138@gmail.com>
2024-04-09 09:44:53 +0530

    Step - I

---
+++ lib/views/login_view.dart
@@ -0,0 +1,110 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
+import 'dart:developer' as devtools show log;
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'Enter your email here',
+            ),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: 'Enter your password here',
+            ),
+          ),
+          TextButton(
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
+
+                try {
+                  await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: email,
+                    password: password,
+                  );
+                  BuildContext currentContext = context;
+                  if (!context.mounted) {
+                    return;
+                  }
+                  Navigator.of(currentContext).pushNamedAndRemoveUntil(
+                    '/notes/',
+                    (route) => false,
+                  );
+                } on FirebaseAuthException catch (e) {
+                  switch (e.code) {
+                    case 'user-not-found':
+                      devtools.log('User not Found');
+                    case 'wrong-password':
+                      devtools.log('Wrong Password');
+                    case 'invalid-email':
+                      devtools.log('Invalid Email');
+                    case 'invalid-credential':
+                      devtools.log('Invalid Credentials');
+                    default:
+                      devtools.log(e.code);
+                  }
+                }
+              },
+              child: const Text('Login')),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/register/', (route) => false);
+            },
+            style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.deepPurple),
+            child: const Text('Not registered yet? register here!'),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/views/register_view.dart
@@ -0,0 +1,109 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
+import 'dart:developer' as devtools show log;
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  // Future<FirebaseApp>? _data;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'Enter your email here',
+            ),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: 'Enter your password here',
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+
+              try {
+                final userCredential = await FirebaseAuth.instance
+                    .createUserWithEmailAndPassword(
+                        email: email, password: password);
+
+                devtools.log(userCredential.toString());
+              } on FirebaseAuthException catch (e) {
+                switch (e.code) {
+                  case 'weak-password':
+                    devtools.log('Weak Password');
+                  case 'email-already-in-use':
+                    devtools.log('Email already in use');
+                  case 'invalid-email':
+                    devtools.log('Invalid Email');
+                  case 'channel-error':
+                    devtools.log("Enter Email and Password");
+                  default:
+                    devtools.log(e.code);
+                }
+              }
+            },
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.deepPurple,
+            ),
+            child: const Text('Register'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pushNamedAndRemoveUntil('/login/', (route) => false);
+            },
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.deepPurple,
+            ),
+            child: const Text('Login here'),
+          ),
+        ],
+      ),
+    );
+  }
+}
